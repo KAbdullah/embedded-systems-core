64171fb5dd7c7764efbf4b3f406a8f68d2865853
Abdullah Khatib <khatibabdullah25@gmail.com>
2026-06-23 23:11:17 -0400

    Completed lab5 part 2

--- learning_C/Lab_5/lab5_2.c
+++ learning_C/Lab_5/lab5_2.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
+void backwardArray();
+
 
 int main() {
+  int arr[6];
+  int *pt = arr;
+  printf("Pass me 6 intgeters (eg., 1,2,3,4,5,6): ");
+  scanf("%d %d %d %d %d %d", pt, pt++, pt++, pt++, pt++, pt++);
+
+  backwardArray(arr);
   
+  int *pt2 = arr;
+  printf("%d %d %d %d %d %d\n", *pt2, *pt2++, *pt2++, *pt2++, *pt2++, *pt2++);
+
+
+}
+
+void backwardArray(int arr[6]) {
+  for (int i = 0; i < 3; i++) {
+    int temp = arr[i];
+    arr[i] = arr[5 - i];
+    arr[5 - i] = temp;
+  }
 }
