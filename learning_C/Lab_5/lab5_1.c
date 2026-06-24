acd75a811792eb528eee7fe205fbfe2eb9b016ca
Abdullah Khatib <khatibabdullah25@gmail.com>
2026-06-23 22:15:01 -0400

    Made lab 5 part 1 less prone to errors like segmentation faults

--- learning_C/Lab_5/lab5_1.c
+++ learning_C/Lab_5/lab5_1.c
@@ -3,17 +3,27 @@
 
 int main () {
   int s, m;
+  //This is a VLA that get's set with m which currently has garbage data because it isn't set yet by the user.
+  //It could cause segmentation faults
   char a[m];
 
-  printf("Please enter the number of spaces you'd like and the number of rows: ");
-  scanf("%d, %d", &s, &m);
+  printf("Please enter the number of spaces you'd like and the number of rows (e.g., 5, 6): ");
+  //Scanf returns the number of successful variables that were set
+  if (scanf("%d, %d", &s, &m) != 2) {
+    printf("Invalid input. \n");
+    //Anything besides return 1 indicates an Error
+    return 1;
+  }
   
 
   for (int i = 0; i < m; i++) {
     for (int j = i; j < s; j++) {
       printf(" ");
     }
-    a[i] = '*';
-    printf("%s\n", a);
+    
+    for (int k = 0; k <= i; k++) {
+      printf("*");
+    }
+    printf("\n");
   }
 }
