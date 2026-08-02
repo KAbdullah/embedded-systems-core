5b45a1007f56aecfe9b6e38054df969c69213a4f
Abdullah Khatib <khatibabdullah25@gmail.com>
2026-08-02 02:36:46 -0400

    Too sleepy right now, I have no clue what the hell I'm doing right now, and so I will go to sleep.

--- os_assignments/Memory_Management/mmu.c
+++ os_assignments/Memory_Management/mmu.c
@@ -34,14 +34,39 @@ int main(int argc, char *argv[]) {
   while (fscanf(fp, "%d", &logicalAddress) == 1) {
 
     //Turn the address into binary format
-    __uint32_t logicalAddressBinary = (__uint32_t) logicalAddress;
+    Address logicalAddressBinary;
+    
+    logicalAddressBinary.address = logicalAddress;
 
     //Mask the bits here so we only care about the first 16 vinary values
     //No need to do this, since the addresses result and the highest 16 bits to be zero, but I'll do it for practice;
 
     //0xF represents the binary equivalent of 1111
     //We take 16 0's and shift them to the highest 16 bits, and clear those bits
-    logicalAddressBinary = logicalAddressBinary & ~(0xFFFF << 16);
+    logicalAddressBinary.page.rightmost = logicalAddressBinary.page.rightmost & ~(0xFFFF << 16);
+
+    FILE *secondary = fopen("BACKING_STORE.bin", "r");
+
+    if (secondary == NULL) {
+      fprintf(stderr, "Couldn't access secondary memory.");
+      return 1;
+    }
+
+    int pageNumber = logicalAddressBinary.page.pageNumber;
+
+    printf("The page number is: %d\n", pageNumber);
+
+    int offset = logicalAddressBinary.page.offset;
+
+    fseek(secondary, (long)pageNumber*256, SEEK_SET);
+
+    signed char physicalAddress[256];
+
+    fread(&physicalAddress, sizeof(signed char), 256, secondary);
+
+    int final_address = (pageNumber * 256) + offset;
+
+    printf("The physical address is: %d\n", (int)physicalAddress[final_address]);
 
   }
   
